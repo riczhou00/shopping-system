f7622a62217250f0764d1da78b1380a67e92dd60
Ricardo Zhou <79975937+riczhou00@users.noreply.github.com>
2022-02-21 23:36:42 +0000

    Add files via upload

---
+++ shopping_cart/app/src/main/java/ipbeja/pdm1/shopping_cart/MainActivity.java
@@ -0,0 +1,14 @@
+package ipbeja.pdm1.shopping_cart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
